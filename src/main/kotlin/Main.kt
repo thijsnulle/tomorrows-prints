f7de52b0d209c590dc15969ebbda4464b49484bf
Thijs Nulle <thijsnulle@Thijss-MacBook-Pro.local>
2023-10-10 17:32:00 +0200

    Simplify constructor of the Poster class

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,14 +2,13 @@ import pipeline.PipelineStep
 import pipeline.steps.PreviewStep
 import pipeline.steps.SocialMediaStep
 import preview.Poster
-import java.nio.file.Paths
 
 fun main() {
     val posters = listOf(
-        Poster(Paths.get("src/main/resources/images/posters").toAbsolutePath().resolve("coffee.png"), "a coffee pot and a cup of coffee on a yellow background, in the style of american modernism, werkstätte, dark cyan and gray, juxtaposed imagery, detailed shading, use of light and shadow, bold-graphic"),
-        Poster(Paths.get("src/main/resources/images/posters").toAbsolutePath().resolve("house.png"), "an abstract square cover depicting a small home with stairs, in the style of sun-soaked colours, konstantinos parthenis, rendered in maya, john pawson, primary colors, site-specific works, laurent chehere"),
-        Poster(Paths.get("src/main/resources/images/posters").toAbsolutePath().resolve("man.png"), "a painting of a person standing in a crowded area near a light house and giant orange disc, in the style of black and white grayscale, digital minimalism, evgeni gordiets, nebulous forms, niko henrichon, grainy, black and white photos, irregular forms"),
-        Poster(Paths.get("src/main/resources/images/posters").toAbsolutePath().resolve("wave.png"), "an image of a poster for the song colors, in the style of minimalist lines, vintage poster style, carpetpunk, chalk art, dramatic diagonals, rainbowcore, unexpected fabric combinations"),
+        Poster("coffee.png", "a coffee pot and a cup of coffee on a yellow background, in the style of american modernism, werkstätte, dark cyan and gray, juxtaposed imagery, detailed shading, use of light and shadow, bold-graphic"),
+        Poster("house.png", "an abstract square cover depicting a small home with stairs, in the style of sun-soaked colours, konstantinos parthenis, rendered in maya, john pawson, primary colors, site-specific works, laurent chehere"),
+        Poster("man.png", "a painting of a person standing in a crowded area near a light house and giant orange disc, in the style of black and white grayscale, digital minimalism, evgeni gordiets, nebulous forms, niko henrichon, grainy, black and white photos, irregular forms"),
+        Poster("wave.png", "an image of a poster for the song colors, in the style of minimalist lines, vintage poster style, carpetpunk, chalk art, dramatic diagonals, rainbowcore, unexpected fabric combinations"),
     )
 
     val pipeline: List<PipelineStep> = listOf(
--- src/main/kotlin/preview/PreviewComposer.kt
+++ src/main/kotlin/preview/PreviewComposer.kt
@@ -1,12 +1,18 @@
 package preview
 
 import java.nio.file.Path
+import java.nio.file.Paths
 
 data class Poster(
     val path: Path,
     val prompt: String,
     val previews: List<Path> = emptyList(),
-)
+) {
+    constructor(fileName: String, prompt: String): this(
+        Paths.get("src/main/resources/images/posters").toAbsolutePath().resolve(fileName),
+        prompt
+    )
+}
 interface PreviewComposer {
     fun compose(poster: Poster): Poster
 }
