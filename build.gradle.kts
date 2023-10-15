996a175984c3b2a7b7e6f56bab395cdcc4cffb11
Sem Duveen <semduveen@gmail.com>
2023-10-15 16:46:41 +0200

    http

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,7 @@ dependencies {
     implementation("com.aallam.openai:openai-client:3.4.0")
     implementation("io.github.cdimascio:dotenv-kotlin:6.4.1")
     implementation("org.seleniumhq.selenium:selenium-java:4.13.0")
+    implementation("com.github.kittinunf.fuel:fuel:3.0.0-alpha1")
 
     runtimeOnly("io.ktor:ktor-client-java-jvm:2.3.4")
 
