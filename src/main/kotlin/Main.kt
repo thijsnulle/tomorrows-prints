ad3c2ecdbffaab0714967e6a7b9d8bf6532ce533
Thijs Nulle <thijsnulle@Thijss-MacBook-Pro.local>
2023-10-18 14:40:33 +0200

    Add CLI to handle the input to the pipeline

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,28 @@
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import pipeline.PipelineStep
 import pipeline.steps.*
 import preview.Poster
+import java.nio.file.Paths
+import kotlin.io.path.exists
+
+data class PosterInfo(val path: String, val prompt: String)
 
 fun main() {
-    val posters = listOf(
-        Poster("coffee.png", "a coffee pot and a cup of coffee on a yellow background, in the style of american modernism, werkstätte, dark cyan and gray, juxtaposed imagery, detailed shading, use of light and shadow, bold-graphic"),
-        Poster("house.png", "an abstract square cover depicting a small home with stairs, in the style of sun-soaked colours, konstantinos parthenis, rendered in maya, john pawson, primary colors, site-specific works, laurent chehere"),
-        Poster("man.png", "a painting of a person standing in a crowded area near a light house and giant orange disc, in the style of black and white grayscale, digital minimalism, evgeni gordiets, nebulous forms, niko henrichon, grainy, black and white photos, irregular forms"),
-        Poster("wave.png", "an image of a poster for the song colors, in the style of minimalist lines, vintage poster style, carpetpunk, chalk art, dramatic diagonals, rainbowcore, unexpected fabric combinations"),
-    )
+    print("Input JSON for the pipeline: ")
+
+    val input = Paths.get(readln().ifEmpty { null } ?: "src/main/resources/default.json").toAbsolutePath()
+    val posters = Gson()
+        .fromJson<List<PosterInfo>>(
+            input.toFile().bufferedReader().use { it.readText() },
+            object : TypeToken<List<PosterInfo>>() {}.type
+        )
+        .map { Poster(it.path, it.prompt) }
+
+    require(posters.all { it.path.exists() }) {
+        "\nThe following image files do not exist in the `posters` folder:\n -" +
+        posters.filterNot { it.path.exists() }.joinToString("\n -") { it.path.toString() }
+    }
 
     val pipeline: List<PipelineStep> = listOf(
         ThemeAllocationStep(),
