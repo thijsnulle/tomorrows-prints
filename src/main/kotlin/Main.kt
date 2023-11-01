32634d2f68071d3ca45dd07d7f2dc34eb9d58e09
Thijs Nulle <thijsnulle@Thijss-MBP.home>
2023-11-01 17:03:45 +0100

    Remove background color matching per poster from SimplePreviewComposer

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,33 +2,45 @@
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import pipeline.steps.*
+import preview.Poster
 import preview.PosterJsonObject
+import preview.SimplePreviewComposer
+import social.pinterest.PinterestContentPrompter
 import social.pinterest.PinterestInfluencer
 import java.nio.file.Paths
 import kotlin.io.path.exists
 
 fun main() {
-    println("Input JSON for the pipeline (leave empty for test file):")
+    val composer = SimplePreviewComposer()
+    val prompter = PinterestContentPrompter()
 
-    val input = Paths.get(readln().ifEmpty { null } ?: "src/main/resources/default.json").toAbsolutePath()
-    val posters = Gson()
-        .fromJson<List<PosterJsonObject>>(
-            input.toFile().bufferedReader().use { it.readText() },
-            object : TypeToken<List<PosterJsonObject>>() {}.type
-        ).map { it.toPoster() }
+    val poster = Poster("waved_shaped.png", "black and white swirls, retroprint style pattern art illustration, in the style of organic biomorphic forms, vintage graphic design, 1960s, intuitive gestures, strong use of negative space, rounded shapes, psychedelic rock")
 
-    require(posters.all { it.path.exists() }) {
-        "\nThe following image files do not exist in the `posters` folder:\n -" +
-        posters.filterNot { it.path.exists() }.joinToString("\n -") { it.path.toString() }
-    }
+    composer.composePreviewsFor(poster)
 
-    val output = listOf(
-        ThemeAllocationStep(),
-        ThumbnailGenerationStep(),
-        PreviewGenerationStep(),
-        PrintFileCreationStep(),
-        PrintfulStep(),
-    ).fold(posters) { current, step -> step.start(current) }
+    println(prompter.ask(poster.prompt))
 
-    PinterestInfluencer().post(output)
+//    println("Input JSON for the pipeline (leave empty for test file):")
+//
+//    val input = Paths.get(readln().ifEmpty { null } ?: "src/main/resources/default.json").toAbsolutePath()
+//    val posters = Gson()
+//        .fromJson<List<PosterJsonObject>>(
+//            input.toFile().bufferedReader().use { it.readText() },
+//            object : TypeToken<List<PosterJsonObject>>() {}.type
+//        ).map { it.toPoster() }
+//
+//    require(posters.all { it.path.exists() }) {
+//        "\nThe following image files do not exist in the `posters` folder:\n -" +
+//        posters.filterNot { it.path.exists() }.joinToString("\n -") { it.path.toString() }
+//    }
+//
+//    val output = listOf(
+//        ThemeAllocationStep(),
+//        ThumbnailGenerationStep(),
+//        PreviewGenerationStep(),
+//        PrintFileCreationStep(),
+//        PrintfulStep(),
+//    ).fold(posters) { current, step -> step.start(current) }
+//
+//    PinterestInfluencer().post(output)
 }
--- src/main/kotlin/preview/SimplePreviewComposer.kt
+++ src/main/kotlin/preview/SimplePreviewComposer.kt
@@ -1,6 +1,5 @@
 package preview
 
-import com.google.common.math.IntMath.pow
 import com.sksamuel.scrimage.ImmutableImage
 import com.sksamuel.scrimage.nio.PngWriter
 import java.awt.Color
@@ -8,25 +7,23 @@ import java.nio.file.Paths
 import java.util.*
 import kotlin.io.path.listDirectoryEntries
 import kotlin.io.path.nameWithoutExtension
-import kotlin.math.abs
 
 const val SIZE = 2048
 const val POSTER_WIDTH = 768
 const val POSTER_HEIGHT = 1152
 const val POSTER_X = (SIZE - POSTER_WIDTH) / 2
 const val POSTER_Y = (SIZE - POSTER_HEIGHT) / 2
-const val GRAY_IMAGE_THRESHOLD = 5
 
 // TODO: add support for horizontal posters
 class SimplePreviewComposer : PreviewComposer() {
 
     private val loader = ImmutableImage.loader()
 
-    private val colors = listOf(
+    private val backgrounds = listOf(
+        Color(255, 255, 255),
         Color(255, 240, 240),
-        Color(240, 255, 240),
         Color(240, 240, 255),
-    )
+    ).map { ImmutableImage.filled(SIZE, SIZE, it) }
 
     private val frames = Paths.get("src/main/resources/images/frames").toAbsolutePath()
         .listDirectoryEntries("*.png").map { loader.fromPath(it) }
@@ -35,36 +32,15 @@ class SimplePreviewComposer : PreviewComposer() {
         val posterImage = loader.fromPath(poster.path).cover(POSTER_WIDTH, POSTER_HEIGHT)
         val outputFolder = Paths.get("src/main/resources/images/previews/${poster.path.nameWithoutExtension}").toAbsolutePath()
 
-        val backgroundColor = backgroundColorFor(posterImage)
-        val background = ImmutableImage.filled(SIZE, SIZE, backgroundColor)
-
-        val previews = frames.map { frame -> background.overlay(posterImage, POSTER_X, POSTER_Y).overlay(frame) }
-            .map { it.output(PngWriter(), outputFolder.resolve("${UUID.randomUUID()}.png")) }
+        val previews = backgrounds.flatMap { background ->
+            frames.map { frame -> background
+                .overlay(posterImage, POSTER_X, POSTER_Y)
+                .overlay(frame)
+                .output(PngWriter(), outputFolder.resolve("${UUID.randomUUID()}.png"))
+            }
+        }
 
         return poster.copy(previews = previews)
-    }
-
-    private fun backgroundColorFor(image: ImmutableImage): Color {
-        val red = image.pixels().map { it.red() }.average().toInt()
-        val green = image.pixels().map { it.green() }.average().toInt()
-        val blue = image.pixels().map { it.blue() }.average().toInt()
-
-        if (isGray(red, green, blue)) return Color.WHITE
-
-        val averageColor = Color(red, green, blue)
-        return colors.minBy { differenceBetweenColors(it, averageColor) }
-    }
-
-    private fun isGray(red: Int, green: Int, blue: Int): Boolean {
-        return abs(red - green) <= GRAY_IMAGE_THRESHOLD &&
-                abs(green - blue) <= GRAY_IMAGE_THRESHOLD &&
-                abs(blue - red) <= GRAY_IMAGE_THRESHOLD
-    }
-
-    private fun differenceBetweenColors(c1: Color, c2: Color): Int {
-        return pow(c1.red - c2.red, 2) +
-                pow(c1.green - c2.green, 2) +
-                pow(c1.blue - c2.blue, 2)
     }
 }
 
