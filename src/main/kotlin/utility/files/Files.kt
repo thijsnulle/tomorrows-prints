15c4db0ddee96663428918844aea497542ab9db2
Thijs Nulle <thijsnulle@Thijss-MacBook-Pro.local>
2023-11-09 08:59:21 +0100

    Remove unused import

--- src/main/kotlin/utility/files/Files.kt
+++ src/main/kotlin/utility/files/Files.kt
@@ -4,7 +4,6 @@ import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import com.google.gson.JsonObject
 import com.google.gson.reflect.TypeToken
-import java.lang.reflect.Type
 import java.nio.file.Path
 import java.nio.file.Paths
 
