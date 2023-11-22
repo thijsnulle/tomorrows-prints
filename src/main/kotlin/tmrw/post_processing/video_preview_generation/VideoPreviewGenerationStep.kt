c0b8def88c1fb8f8926db2703f520afebe54ee44
Thijs Nulle <thijsnulle@Thijss-MacBook-Pro.local>
2023-11-22 14:01:40 +0100

    Add video preview with glitch effect

--- src/main/kotlin/tmrw/post_processing/video_preview_generation/VideoPreviewGenerationStep.kt
+++ src/main/kotlin/tmrw/post_processing/video_preview_generation/VideoPreviewGenerationStep.kt
@@ -1,18 +1,28 @@
 package tmrw.post_processing.video_preview_generation
 
+import com.sksamuel.scrimage.ImmutableImage
+import com.sksamuel.scrimage.nio.JpegWriter
+import com.sksamuel.scrimage.nio.PngWriter
 import tmrw.model.Print
 import tmrw.pipeline.preview_generation.FramedPreviewGenerator
 import tmrw.post_processing.PostProcessingAggregate
 import tmrw.post_processing.PostProcessingStep
 import tmrw.utils.Files
 import tmrw.utils.Files.Companion.batchFolder
+import java.nio.file.Path
 import java.util.*
 import kotlin.io.path.*
+import kotlin.random.Random
 
 const val PRINTS_PER_VIDEO_PREVIEW = 30
 const val FRAME_RATE_VIDEO_PREVIEW = 5
 
 class VideoPreviewGenerationStep: PostProcessingStep() {
+
+    private val loader = ImmutableImage.loader()
+    private val random = Random.Default
+    private val writer = JpegWriter.compression(85).withProgressive(true)
+
     override fun process(prints: List<Print>, aggregate: PostProcessingAggregate): PostProcessingAggregate {
         val videoPreviewsFolder = Files.previews.batchFolder(prints.first()).parent.resolve("videos").toAbsolutePath()
 
@@ -24,6 +34,13 @@ class VideoPreviewGenerationStep: PostProcessingStep() {
 
         videoPreviewsFolder.createDirectories()
 
+        val cyclePreviews = createCyclePreviews(prints, videoPreviewsFolder)
+        val glitchPreviews = prints.map { createGlitchPreview(it, videoPreviewsFolder) }
+
+        return aggregate.copy(videoPreviews = cyclePreviews + glitchPreviews)
+    }
+
+    private fun createCyclePreviews(prints: List<Print>, videoPreviewsFolder: Path): List<Path> {
         val temporaryDirectory = java.nio.file.Files.createTempDirectory("")
         val previewGenerator = FramedPreviewGenerator(previewFolder = temporaryDirectory, createSquarePreviews = true)
 
@@ -33,7 +50,7 @@ class VideoPreviewGenerationStep: PostProcessingStep() {
             .shuffled()
             .chunked(PRINTS_PER_VIDEO_PREVIEW)
 
-        val videoPreviews = previewChunks.map { previews ->
+        return previewChunks.map { previews ->
             if (previews.size != PRINTS_PER_VIDEO_PREVIEW) return@map null
 
             temporaryDirectory.listDirectoryEntries("*.jpeg").forEach { it.deleteIfExists() }
@@ -57,7 +74,51 @@ class VideoPreviewGenerationStep: PostProcessingStep() {
 
             Path(output)
         }.filterNotNull()
+    }
 
-        return aggregate.copy(videoPreviews = videoPreviews)
+    private fun createGlitchPreview(print: Print, videoPreviewsFolder: Path): Path {
+        val skippedFrames = 6
+        val totalFrames = 96
+
+        val image = loader.fromPath(print.path)
+        val temporaryDirectory = java.nio.file.Files.createTempDirectory("")
+
+        (1..skippedFrames).forEach {
+            image.output(writer, temporaryDirectory.resolve("$it.jpeg"))
+            image.output(writer, temporaryDirectory.resolve("${totalFrames - it}.jpeg"))
+        }
+
+        val frames = (skippedFrames..(totalFrames / 2))
+
+        frames.fold(image) { aggregateImage, frame ->
+            val scaleFactor = random.nextDouble(0.25, 0.75)
+            val scaledWidth = (image.width * scaleFactor).toInt()
+            val scaledHeight = (image.height * scaleFactor).toInt()
+
+            val x = random.nextInt(-scaledWidth / 2, image.width - scaledWidth / 2)
+            val y = random.nextInt(-scaledHeight / 2, image.height - scaledHeight / 2)
+
+            aggregateImage
+                .overlay(image.scale(scaleFactor), x, y)
+                .also { it.output(writer, temporaryDirectory.resolve("$frame.jpeg")) }
+                .also { it.output(writer, temporaryDirectory.resolve("${totalFrames - frame}.jpeg")) }
+        }
+
+        val output = "$videoPreviewsFolder/${UUID.randomUUID()}.mp4"
+        val commandsToGenerateCompleteVideoPreview = listOf(
+            "ffmpeg", "-y",
+            "-framerate", "12",
+            "-i", "%d.jpeg",
+            "-c:v", "libx264",
+            "-pix_fmt", "yuv420p",
+            output,
+        )
+
+        ProcessBuilder(commandsToGenerateCompleteVideoPreview)
+            .directory(temporaryDirectory.toFile())
+            .start()
+            .waitFor()
+
+        return Path(output)
     }
 }
