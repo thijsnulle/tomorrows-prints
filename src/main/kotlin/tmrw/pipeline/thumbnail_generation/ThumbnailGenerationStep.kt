d45a45b575426915ac7167167ed40062a253a318
Thijs Nulle <thijsnulle@Thijss-MacBook-Pro.local>
2023-11-30 08:54:42 +0100

    Simplify the ThumbnailGenerationStep

--- src/main/kotlin/tmrw/pipeline/thumbnail_generation/ThumbnailGenerationStep.kt
+++ src/main/kotlin/tmrw/pipeline/thumbnail_generation/ThumbnailGenerationStep.kt
@@ -2,14 +2,12 @@ package tmrw.pipeline.thumbnail_generation
 
 import tmrw.model.Print
 import tmrw.pipeline.PipelineStep
+import tmrw.pipeline.preview_generation.FramedPreviewGenerator
+import tmrw.utils.Files
 
 class ThumbnailGenerationStep: PipelineStep() {
-    private val generator = ThumbnailGenerator()
+    private val generator = FramedPreviewGenerator(previewFolder = Files.thumbnails, createSquarePreviews = true)
 
-    override fun process(print: Print): Print {
-        val thumbnailPath = generator.generateThumbnail(print)
-        return print.copy(thumbnail = thumbnailPath.toString())
-    }
-
+    override fun process(print: Print): Print = print.copy(thumbnail = generator.generate(print).random().toString())
     override fun shouldSkip(print: Print): Boolean = print.thumbnail.isNotEmpty()
 }
