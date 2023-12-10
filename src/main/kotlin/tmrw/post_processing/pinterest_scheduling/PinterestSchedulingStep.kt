f5c5d41a97016ede7a06691bbddfa641ba0b1a8a
Thijs Nulle <thijsnulle@Thijss-MBP.home>
2023-12-10 23:43:40 +0100

    Add correct titles and links to video previews

--- src/main/kotlin/tmrw/post_processing/pinterest_scheduling/PinterestSchedulingStep.kt
+++ src/main/kotlin/tmrw/post_processing/pinterest_scheduling/PinterestSchedulingStep.kt
@@ -9,6 +9,8 @@ import java.time.LocalDateTime
 import java.time.LocalTime
 import kotlin.io.path.createDirectories
 import kotlin.io.path.exists
+import kotlin.io.path.name
+import kotlin.io.path.nameWithoutExtension
 
 const val MAXIMUM_SIZE_BULK_UPLOAD_PINS = 200
 const val INTERVAL_BETWEEN_POST: Long = 30
@@ -40,11 +42,13 @@ class PinterestSchedulingStep(val batch: String): PostProcessingStep() {
 
         val totalTime = previewCsvRows.size * INTERVAL_BETWEEN_POST
         val videoPreviewCsvRows = aggregate.videoPreviewUrls.mapIndexed { index, videoPreviewUrl ->
-            val publishDate = initialDateTime.plusMinutes(index * totalTime / aggregate.videoPreviewUrls.size)
+            val printUuid = getUUID(videoPreviewUrl) ?: return@mapIndexed null
+            val print = prints.firstOrNull { it.path.nameWithoutExtension == printUuid } ?: return@mapIndexed null
 
-            // TODO: add store URL
-            "\"Exclusive Print Showcase • Tomorrow's Prints #${index + 1}\",$videoPreviewUrl,All Posters,,,#storeUrl,$publishDate,"
-        }
+            val title = "${title(videoPreviewUrl)} ${print.title} • Tomorrow's Prints"
+            val publishDate = initialDateTime.plusMinutes(index * totalTime / aggregate.videoPreviewUrls.size)
+            "\"$title\",$videoPreviewUrl,All Posters,,,${print.listingUrl},$publishDate,"
+        }.filterNotNull()
 
         (showCaseCsvRows + previewCsvRows + videoPreviewCsvRows)
             .chunked(MAXIMUM_SIZE_BULK_UPLOAD_PINS)
@@ -57,4 +61,21 @@ class PinterestSchedulingStep(val batch: String): PostProcessingStep() {
 
         return aggregate
     }
+}
+
+private fun getUUID(str: String): String? {
+    val printUuidRegex = Regex("([a-f\\d]{8}-[a-f\\d]{4}-[a-f\\d]{4}-[a-f\\d]{4}-[a-f\\d]{12})")
+    val matches = printUuidRegex.findAll(str).toList()
+
+    return matches.getOrNull(matches.size - 2)?.groupValues?.get(1)
+}
+
+private fun title(fileName: String) = when {
+    fileName.contains("carousel") -> "Carousel Wonders:"
+    fileName.contains("colour-rotation") -> "All Shades of"
+    fileName.contains("cycle") -> "Image Symphony:"
+    fileName.contains("hue-rotate") -> "The Full Spectrum:"
+    fileName.contains("glitch") -> "Pulsating of"
+    fileName.contains("zoom") -> "Zoom Magic:"
+    else -> "Simply Stunning: "
 }
