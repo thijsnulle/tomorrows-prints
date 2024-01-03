3818fdec4d7fb5c1ef456bfefa426a6075c01c71
Thijs Nulle <thijsnulle@Thijss-MacBook-Pro.local>
2024-01-03 17:33:31 +0100

    Store most recent post processing aggregate

--- src/main/kotlin/tmrw/post_processing/PostProcessingStep.kt
+++ src/main/kotlin/tmrw/post_processing/PostProcessingStep.kt
@@ -1,15 +1,35 @@
 package tmrw.post_processing
 
+import com.google.gson.JsonArray
+import com.google.gson.JsonObject
 import io.github.oshai.kotlinlogging.KotlinLogging
 import tmrw.model.Print
+import tmrw.utils.JsonMappable
 import java.nio.file.Path
+import kotlin.io.path.Path
 import kotlin.time.Duration
 import kotlin.time.measureTimedValue
 
 data class PostProcessingAggregate(
     val videoPreviews: List<Path> = emptyList(),
     val videoPreviewUrls: List<String> = emptyList(),
-)
+): JsonMappable {
+    override fun toJson(): JsonObject {
+        val jsonObject = JsonObject()
+
+        jsonObject.add("videoPreviews", JsonArray().also { videoPreviews.forEach { preview -> it.add(preview.toString()) }})
+        jsonObject.add("videoPreviewUrls", JsonArray().also { videoPreviewUrls.forEach { preview -> it.add(preview) }})
+
+        return jsonObject
+    }
+}
+
+data class JsonPostProcessingAggregate(
+    val videoPreviews: List<String>,
+    val videoPreviewUrls: List<String>,
+) {
+    fun toAggregate() = PostProcessingAggregate(videoPreviews.map { Path(it) }, videoPreviewUrls)
+}
 
 abstract class PostProcessingStep {
 
